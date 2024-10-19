38b5b1b4ce04856e2968d4be54d39f91278eafb2
aeciojose <aeciojunior45@gmail.com>
2024-10-19 08:51:52 -0300

    funcionalidades_basicas

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import requests 
+
 lista_ceps = ['01153000', '20050000', '70714020']
 lista_endereco = []
 
@@ -7,3 +8,5 @@ for cep in lista_ceps:
     req = requests.get(url, timeout=3)
     endereco = req.json()
     lista_endereco.append([endereco["cep"], endereco["logradouro"], endereco["bairro"], endereco["localidade"]])
+for end in lista_endereco:
+    print(end)
