ca607463f8ba6b3c0eeede5ea40cd49a9c1ed35d
aeciojosenot <aeciojunior45@gmail.com>
2024-10-20 09:28:22 -0300

    organizando_defs

--- consultando_ceps/api.py
+++ consultando_ceps/api.py
@@ -5,10 +5,13 @@ lista_ceps = []
 lista_ceps.append('{}'.format(consultar_cep))
 lista_endereco = []
 
-for cep in lista_ceps:
-    url = 'https://viacep.com.br/ws/{}/json/'.format(cep)
-    req = requests.get(url, timeout=3)
-    endereco = req.json()
-    lista_endereco.append([endereco["cep"], endereco["logradouro"], endereco["bairro"], endereco["localidade"]])
-for end in lista_endereco:
-    print(end)
+def consultar_cep():
+    for cep in lista_ceps:
+        url = 'https://viacep.com.br/ws/{}/json/'.format(cep)
+        req = requests.get(url, timeout=3)
+        endereco = req.json()
+        lista_endereco.append([endereco["cep"], endereco["logradouro"], endereco["bairro"], endereco["localidade"]])
+    for end in lista_endereco:
+        print(end)
+
+consultar_cep()
